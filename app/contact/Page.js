4b30c213b722051c929074362705a0fe44047815
Ridhi <ridhisureka5@gmail.com>
2025-05-19 21:40:40 +0530

    co

--- app/contact/Page.js
+++ app/contact/Page.js
@@ -1,7 +1,23 @@
 'use client';
 import Part from '../ParticlesBackground/Page';
-
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 export default function Contacts() {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_smd9cqm', 'template_bflv74a', form.current, 'YUH1-BDvoNGEffAiD')
+      .then((result) => {
+          console.log(result.text);
+          alert("Message sent successfully!");
+      }, (error) => {
+          console.log(error.text);
+          alert("Failed to send message. Please try again.");
+      });
+
+    e.target.reset(); // reset form after submission
+  };
+
   return (
     <div className="  bg-gradient-to-r from-gray-800 to-black text-white min-h-screen flex justify-between">
       {/* Particle background */}
@@ -11,22 +27,28 @@ export default function Contacts() {
       <div className=" z-10 w-full max-w-6xl mx-auto p-6 grid grid-cols-1 md:grid-cols-2 gap-30 items-center">
         
         {/* Contact Form - Left */}
-        <form className="bg-white text-black p-6 rounded-2xl shadow-lg space-y-4 ">
+        <form ref={form} onSubmit={sendEmail} className="bg-white text-black p-6 rounded-2xl shadow-lg space-y-4 ">
           <h3 className="text-2xl font-semibold">Send a Message</h3>
           <input
             type="text"
+            name="name"
             placeholder="Your Name"
             className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black"
+            required
           />
           <input
             type="email"
+            name="email"
             placeholder="Your Email"
             className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black"
+            required
           />
           <textarea
+            name="message"
             placeholder="Your Message"
             rows="4"
             className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-black"
+            required
           />
           <button
             type="submit"
@@ -44,10 +66,6 @@ export default function Contacts() {
           <p className="text-lg">🧠 LeetCode: <a href="https://leetcode.com/ridhi__sureka1" target="_blank" className="text-yellow-400 hover:underline">leetcode.com/ridhi__sureka1</a></p>
         </div>
       </div>
-
-     
     </div>
-    
-    
   );
 }
